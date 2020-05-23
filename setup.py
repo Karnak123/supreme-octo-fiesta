97c2ff7ab746eb7bf1c0ab84ff8c54d4385dab01
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2020-05-24 00:06:20 +0530

    removed redundant parenthesis
    Signed-off-by: Sagnik Mazumder <sagnikmazumdar37@gmail.com>

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setup(name='Matricx',
       version='0.0.1',
       author='Sagnik Mazumder, Sucheta Roy',
       author_email='sagnikmazumdar37@gmail.com, sucheta56roy@gmail.com',
-      description=('Python package to implement basic matrix operations'),
+      description='Python package to implement basic matrix operations',
       license='MIT',
       packages=['Matricx'])
