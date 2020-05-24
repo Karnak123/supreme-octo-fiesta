3ab921ed33b8d213126eb66c7405213ab1fb2a4f
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2020-05-24 13:53:12 +0530

    changed version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='Matricx',
-      version='0.0.1',
+      version='0.0.2',
       author='Sagnik Mazumder, Sucheta Roy',
       author_email='sagnikmazumdar37@gmail.com, sucheta56roy@gmail.com',
       description='Python package to implement basic matrix operations',
